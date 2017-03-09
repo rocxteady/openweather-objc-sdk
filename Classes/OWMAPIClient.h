9184932090839a7d6c735888a0fba34cfce59cdc
Ulaş Sancak <ulas@oncusancak-macbook-pro.local>
2017-03-09 12:55:10 +0300

    API client class created.

---
+++ Classes/OWMAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  OWMAPIClient.h
+//  OpenWeatherMapExample
+//
+//  Created by Ulaş Sancak on 09/03/2017.
+//  Copyright © 2017 Ulaş Sancak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OWMAPIClient : NSObject
+
+@end
---
+++ Classes/OWMAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  OWMAPIClient.m
+//  OpenWeatherMapExample
+//
+//  Created by Ulaş Sancak on 09/03/2017.
+//  Copyright © 2017 Ulaş Sancak. All rights reserved.
+//
+
+#import "OWMAPIClient.h"
+
+@implementation OWMAPIClient
+
+@end
