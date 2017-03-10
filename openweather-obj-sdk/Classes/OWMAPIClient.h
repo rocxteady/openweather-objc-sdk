1cd425b616959ef9d53ee927a2f18d42a08e5524
Ulaş Sancak <ulas@oncusancak-macbook-pro.local>
2017-03-11 01:48:13 +0300

    Current weather api client method declared.

--- openweather-obj-sdk/Classes/OWMAPIClient.h
+++ openweather-obj-sdk/Classes/OWMAPIClient.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "OWMCoordinates.h"
 
 @interface OWMAPIClient : NSObject
 
@@ -14,6 +15,25 @@ typedef void(^OWMClientResponseBlock)(NSDictionary *response, NSError *error);
 
 + (OWMAPIClient *)client;
 
-- (void)getWeatherWithBlock:(OWMClientResponseBlock)block;
+//Current weather
+
+- (void)getWeatherByCityName:(NSString *)cityName block:(OWMClientResponseBlock)block;
+
+- (void)getWeatherByCityName:(NSString *)cityName countryCode:(NSString *)countryCode block:(OWMClientResponseBlock)block;
+
+- (void)getWeatherByCityID:(NSUInteger)cityID block:(OWMClientResponseBlock)block;
+
+- (void)getWeatherByCityIDs:(NSArray *)cityIDs block:(OWMClientResponseBlock)block;
+
+- (void)getWeatherByCoordinates:(OWMCoordinates *)coordinates block:(OWMClientResponseBlock)block;
+
+- (void)getWeatherByRectangleZone:(NSString *)zone block:(OWMClientResponseBlock)block;
+
+- (void)getWeatherByCycleZoneWithCoordinates:(OWMCoordinates *)coordinates countOfCity:(NSUInteger)count block:(OWMClientResponseBlock)block;
+
+- (void)getWeatherByCityZIPCode:(NSString *)ZIPCode block:(OWMClientResponseBlock)block;
+
+- (void)getWeatherByCityZIPCode:(NSString *)ZIPCode countryCode:(NSString *)countryCode block:(OWMClientResponseBlock)block;
+
 
 @end
