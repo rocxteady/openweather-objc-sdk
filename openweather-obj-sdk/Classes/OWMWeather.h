18fe946828afca0321c452c7433f8b4c85bfaedd
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2017-03-11 04:01:46 +0300

    Getting weather by city name and city ids.

--- openweather-obj-sdk/Classes/OWMWeather.h
+++ openweather-obj-sdk/Classes/OWMWeather.h
@@ -14,19 +14,31 @@
 #import "OWMClouds.h"
 #import "OWMSys.h"
 
+@protocol OWMWeather
+@end
+
+@class OWMWeather;
+@class OWMWeatherArrayResult;
+
+typedef void(^OWMWeatherBlock)(OWMWeather *weatherData, NSError *error);
+typedef void(^OWMWeatherArrayResultBlock)(OWMWeatherArrayResult *result, NSError *error);
+
 @interface OWMWeather : OWMBaseModel
 
 @property (strong, nonatomic) OWMCoordinates *coordinates;
-@property (strong, nonatomic) OWMWeatherInfo *weatherInfo;
+@property (strong, nonatomic) NSArray <OWMWeatherInfo> *weatherInfos;
 @property (strong, nonatomic) NSString *base;
 @property (strong, nonatomic) OWMMain *main;
 @property (assign, nonatomic) NSUInteger visibility;
-@property (strong, nonatomic) OWMWind *winds;
+@property (strong, nonatomic) OWMWind *wind;
 @property (strong, nonatomic) OWMClouds *clouds;
 @property (assign, nonatomic) double dt;
 @property (strong, nonatomic) OWMSys *sys;
 @property (assign, nonatomic) NSUInteger weatherID;
-@property (strong, nonatomic) NSString *country;
-@property (assign, nonatomic) NSUInteger cond;
+@property (strong, nonatomic) NSString *name;
+
++ (void)getWeatherByCityName:(NSString *)cityName block:(OWMWeatherBlock)block;
+
++ (void)getWeatherByCityIds:(NSArray *)cityIds block:(OWMWeatherArrayResultBlock)block;
 
 @end
--- openweather-obj-sdk/Classes/OWMWeather.m
+++ openweather-obj-sdk/Classes/OWMWeather.m
@@ -7,13 +7,35 @@
 //
 
 #import "OWMWeather.h"
+#import "OWMAPIClient.h"
+#import "OWMWeatherArrayResult.h"
 
 @implementation OWMWeather
+
++ (void)getWeatherByCityName:(NSString *)cityName block:(OWMWeatherBlock)block {
+    [[OWMAPIClient client] getWeatherByCityName:cityName block:^(NSDictionary *response, NSError *error) {
+        OWMWeather *weatherData = nil;
+        if (!error) {
+            weatherData = [[OWMWeather alloc] initWithDictionary:response error:&error];
+        }
+        block(weatherData, error);
+    }];
+}
+
++ (void)getWeatherByCityIds:(NSArray *)cityIds block:(OWMWeatherArrayResultBlock)block {
+    [[OWMAPIClient client] getWeatherByCityIDs:cityIds block:^(NSDictionary *response, NSError *error) {
+        OWMWeatherArrayResult *result = nil;
+        if (!error) {
+            result = [[OWMWeatherArrayResult alloc] initWithDictionary:response error:&error];
+        }
+        block(result, error);
+    }];
+}
 
 + (JSONKeyMapper *)keyMapper {
     return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:@{
                                                                   @"coordinates": @"coord",
-                                                                  @"weatherInfo": @"weather",
+                                                                  @"weatherInfos": @"weather",
                                                                   @"base": @"base",
                                                                   @"main": @"main",
                                                                   @"visibility":  @"visibility",
@@ -23,7 +45,7 @@
                                                                   @"sys": @"sys",
                                                                   @"weatherID":  @"id",
                                                                   @"name": @"name",
-                                                                  @"cond": @"cond"
+                                                                  @"cod": @"cod"
                                                                   }];
 }
 
