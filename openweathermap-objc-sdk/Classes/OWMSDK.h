5a84ed4d542d929c051307ada207db49ea9a10cd
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2017-03-16 23:50:31 +0300

    Public headers updated.

--- openweathermap-objc-sdk/Classes/OWMSDK.h
+++ openweathermap-objc-sdk/Classes/OWMSDK.h
@@ -8,3 +8,9 @@
 
 #import <Foundation/Foundation.h>
 #import "OWMClient.h"
+#import "OWMAPIClient.h"
+#import "OWMWeather.h"
+#import "OWMDailyForecastWeather.h"
+#import "OWMWeatherArrayResult.h"
+#import "OWMDailyForecastResult.h"
+#import "OWMCoordinates.h"
